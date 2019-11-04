d1f724415415ac09cbddf00e6e77028f7a9f43b9
HappyFatOtaku <939561545@qq.com>
2019-11-04 19:30:34 +0800

    第一次提交

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="back-top">
+      <img src="~assets/img/common/top.png" alt="返回顶部" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackTop'
+}
+</script>
+
+<style scoped>
+.back-top {
+  position: fixed;
+  right: 8px;
+  bottom: 55px;
+}
+
+.back-top img {
+  width: 43px;
+  height: 43px;
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="#ff5777">
+      <!-- src 这里的路径使用路径别名需要在别名前加一个 ~ 符号 -->
+      <img src="~assets/img/tabbar/home.svg" slot="item-icon" />
+      <img src="~assets/img/tabbar/home_active.svg" slot="item-icon-active" />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="#ff5777">
+      <img src="~assets/img/tabbar/category.svg" slot="item-icon" />
+      <img src="~assets/img/tabbar/category_active.svg" slot="item-icon-active" />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/shopcart" activeColor="#ff5777">
+      <img src="~assets/img/tabbar/shopcart.svg" slot="item-icon" />
+      <img src="~assets/img/tabbar/shopcart_active.svg" slot="item-icon-active" />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="#ff5777">
+      <img src="~assets/img/tabbar/profile.svg" slot="item-icon" />
+      <img src="~assets/img/tabbar/profile_active.svg" slot="item-icon-active" />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+// import 这里的路径可以直接使用路径别名即可
+import TabBar from 'components/common/tabbar/Tabbar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
